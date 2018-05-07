c68316f6df77d0d006229abdc0934e7517b7ccb0
Lars Tobias Skjong-Børsting <larstobi@relatime.no>
2018-05-07 10:50:59 +0200

    Added security group for ElastiCache module.

--- elasticache/main.tf
+++ elasticache/main.tf
@@ -15,5 +15,24 @@ resource "aws_elasticache_cluster" "main" {
   num_cache_nodes      = "${var.num_cache_nodes}"
   az_mode              = "${var.az_mode}"
   port                 = "${var.port}"
-  tags                 = "${var.tags}"
+
+  tags = "${var.tags}"
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.prefix}-memcached-sg"
+  description = "Terraformed security group."
+  vpc_id      = "${var.vpc_id}"
+
+  tags = "${merge(var.tags, map("Name", "${var.prefix}-memcached-sg"))}"
+}
+
+resource "aws_security_group_rule" "ingress" {
+  description              = "Terraformed security group rule."
+  security_group_id        = "${aws_security_group.main.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "${var.port}"
+  to_port                  = "${var.port}"
+  source_security_group_id = "${var.source_security_group_id}"
 }
