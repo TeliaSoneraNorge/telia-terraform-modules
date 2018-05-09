e7015f329d9d7c05dda2c453c8ac7c262d1a30ca
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-05-09 13:52:20 +0200

    add egress to ec2 instances sg

--- ecs/spotfleet-cluster/main.tf
+++ ecs/spotfleet-cluster/main.tf
@@ -79,6 +79,14 @@ resource "aws_security_group_rule" "ingress" {
   source_security_group_id = "${element(var.load_balancers, count.index)}"
 }
 
+resource "aws_security_group_rule" "egress-all" {
+  from_port = 0
+  protocol = "all"
+  security_group_id = "${aws_security_group.ecs-instances.id}"
+  to_port = 0
+  type = "egress"
+}
+
 resource "aws_iam_instance_profile" "ecs" {
   name = "${var.prefix}-ecs-instance"
   role = "${aws_iam_role.ec2-instance.name}"
