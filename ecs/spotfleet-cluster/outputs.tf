14e4c4b0d0d930891f2fd6bc9c6dcb8e342292fc
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-05-13 13:01:58 +0200

    fix outputs to use correct role

--- ecs/spotfleet-cluster/outputs.tf
+++ ecs/spotfleet-cluster/outputs.tf
@@ -6,15 +6,15 @@ output "id" {
 }
 
 output "role_name" {
-  value = "${aws_iam_role.spotfleet.name}"
+  value = "${aws_iam_role.ec2-instance.name}"
 }
 
 output "role_arn" {
-  value = "${aws_iam_role.spotfleet.arn}"
+  value = "${aws_iam_role.ec2-instance.arn}"
 }
 
 output "role_id" {
-  value = "${aws_iam_role.spotfleet.id}"
+  value = "${aws_iam_role.ec2-instance.id}"
 }
 
 output "security_group_id" {
