6528a83d159c6eedd7b75eb4bce9418f62a10995
glennbech <glenn.bech@gmail.com>
2018-05-23 14:29:21 +0200

    lint2

--- splunk-forwarder/example/main.tf
+++ splunk-forwarder/example/main.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 module "cloudwatch_splunk_lambda_subscription" {
-  source = "../../splunk-forwarder"
+  source          = "../../splunk-forwarder"
   log_group_names = "${keys(local.subscriptions)}"
   filter_patterns = "${values(local.subscriptions)}"
 }
--- splunk-forwarder/main.tf
+++ splunk-forwarder/main.tf
@@ -1,5 +1,6 @@
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
+
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -18,8 +19,9 @@ EOF
 }
 
 resource "aws_iam_policy" "allow_cloudwatch" {
-  name = "cw_for_lambda"
+  name        = "cw_for_lambda"
   description = "Lets the Lambda function write to cloudwatch"
+
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -42,8 +44,9 @@ EOF
 }
 
 resource "aws_iam_policy" "allow_s3_puts" {
-  name = "s3_for_lambda"
+  name        = "s3_for_lambda"
   description = "Lets the Lambda function write to a any bucket"
+
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -61,12 +64,12 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_cw_attach" {
-  role = "${aws_iam_role.lambda_role.name}"
+  role       = "${aws_iam_role.lambda_role.name}"
   policy_arn = "${aws_iam_policy.allow_cloudwatch.arn}"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_attach" {
-  role = "${aws_iam_role.lambda_role.name}"
+  role       = "${aws_iam_role.lambda_role.name}"
   policy_arn = "${aws_iam_policy.allow_s3_puts.arn}"
 }
 
@@ -74,14 +77,15 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_attach" {
 // infrastructure.
 
 resource "aws_lambda_function" "bucket_forwarder" {
-  s3_bucket = "telia-common-logs-prod-lambda"
-  s3_key = "cloudwatch-logs-remote-bucket-1.0-SNAPSHOT.zip"
+  s3_bucket     = "telia-common-logs-prod-lambda"
+  s3_key        = "cloudwatch-logs-remote-bucket-1.0-SNAPSHOT.zip"
   function_name = "telia-common-logs-lambda"
-  role = "${aws_iam_role.lambda_role.arn}"
-  handler = "com.telia.aws.cloudwatchtoremotebucket.Handler::handleRequest"
-  runtime = "java8"
-  timeout = 60
-  memory_size = 512
+  role          = "${aws_iam_role.lambda_role.arn}"
+  handler       = "com.telia.aws.cloudwatchtoremotebucket.Handler::handleRequest"
+  runtime       = "java8"
+  timeout       = 60
+  memory_size   = 512
+
   environment {
     variables = {
       bucket_name = "telia-common-logs-prod-application-logs"
@@ -90,23 +94,20 @@ resource "aws_lambda_function" "bucket_forwarder" {
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id = "AllowExecutionFromCloudWatch"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.bucket_forwarder.function_name}"
-  principal = "logs.amazonaws.com"
+  principal     = "logs.amazonaws.com"
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "test_lambdafunction_logfilter" {
-  count = "${length(var.filter_patterns)}"
-  name = "lambdafunction_logfilter_${element(var.log_group_names, count.index)}"
-  log_group_name = "${element(var.log_group_names, count.index)}"
-  filter_pattern = "${element(var.filter_patterns, count.index)}"
+  count           = "${length(var.filter_patterns)}"
+  name            = "lambdafunction_logfilter_${element(var.log_group_names, count.index)}"
+  log_group_name  = "${element(var.log_group_names, count.index)}"
+  filter_pattern  = "${element(var.filter_patterns, count.index)}"
   destination_arn = "${aws_lambda_function.bucket_forwarder.arn}"
 }
 
 output "aws_lambda_arn" {
   value = "${aws_lambda_function.bucket_forwarder.arn}"
 }
-
-
-
