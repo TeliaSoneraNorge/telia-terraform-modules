890d52be1fb69820e278936f6b47469a9c24db5e
glennbech <glenn.bech@gmail.com>
2018-05-28 13:26:59 +0200

    cleanup2

--- splunk-ingestable-s3-bucket/example/main.tf
+++ splunk-ingestable-s3-bucket/example/main.tf
@@ -1,13 +1,7 @@
-// Creates S3 and other resources that enables Splunk to use SQS bases S3 ingestion
-resource "random_integer" "id" {
-  max = 10000
-  min = 0
-}
-
 module "splunk_ingestable_s3_bucket" {
   source              = "../"
   splunk_account_id   = "245629465185"
-  log_bucket_name     = "splunk-s3-test-${random_integer.id.result}"
+  log_bucket_name     = "splunk-s3-test"
   aws_organization_id = "o-wpral5esws"
 
   tags = {
