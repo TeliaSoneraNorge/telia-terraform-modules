83f912597e29f3b8a57bb5cc2ec4cb1d64bff587
glennbech <glenn.bech@gmail.com>
2018-06-14 19:18:14 +0200

    Fix for #74

--- ecs/example/main.tf
+++ ecs/example/main.tf
@@ -222,12 +222,13 @@ module "fargate" {
   private_subnet_ids    = "${module.vpc.private_subnet_ids}"
   cluster_id            = "${aws_ecs_cluster.fargate_cluster.id}"
   task_definition_image = "crccheck/hello-world:latest"
-  container_port        = "8080"
-  container_protocol    = "HTTP"
+  // port, default protocol is HTTP
+  task_container_port   = "8080"
 
   health_check {
     path = "/"
   }
 
   tags = "${var.tags}"
+  lb_arn = "${module.alb.arn}"
 }
